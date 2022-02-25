478f43a3d0d4e29a30dba4f55f6765e85dc40f70
Vijay Saayi R <Vijay.Saayi@microsoft.com>
2022-02-25 19:35:25 +0530

    Adding support for local development

--- src/appsvc_profiler/constants.py
+++ src/appsvc_profiler/constants.py
@@ -1,19 +1,50 @@
 import os
+
 class CodeProfilerConstants():
     APPSVC_PROFILER_VERSION="1.0.0"
     APP_SETTING_TO_ENABLE_CODE_PROFILER = "APPSETTING_WEBSITE_ENABLE_DEFAULT_CODE_PROFILER" 
     APP_SETTING_TO_SET_SAVE_TRACE_MAX_TIMEOUT = "APPSETTING_WEBSITE_CODE_PROFILER_SAVE_TRACE_TIMEOUT"
     
-    CODE_PROFILER_LOGS_DIR = "/home/LogFiles/CodeProfiler"
-    CODE_PROFILER_LOCKS_DIR = "/home/site/CodeProfiler/StatusFiles"
+    @property
+    def CODE_PROFILER_LOGS_DIR(self):
+        log_dir = "home/LogFiles/CodeProfiler"
+        return self._get_directory(log_dir)
+    
+    @property
+    def CODE_PROFILER_LOCKS_DIR(self):
+        log_dir = "home/site/CodeProfiler/StatusFiles"
+        return self._get_directory(log_dir)
+    
+    def _check_if_local_development_is_used(self):
+        is_local_development_env = os.getenv(self.LOCAL_DEVELOP_ENV_NAME, 'false')
+        return (str(is_local_development_env) == "1"
+                or str(is_local_development_env).lower() == "true")
+    
+    def _get_directory(self, logs_directory):
+        is_local_development = self._check_if_local_development_is_used()
+        local_development_dir = os.getenv(self.LOCAL_DEVELOP_HOME_DIR, '/tmp/kududirs')
+        if(is_local_development):
+            return f"{local_development_dir}/{logs_directory}"        
+        return f"/{logs_directory}"
+    
     CODE_PROFILER_TRACE_FILENAME="profiler_trace.json"
-    CODE_PROFILER_TRACE_NAME = f"{CODE_PROFILER_LOGS_DIR}/{CODE_PROFILER_TRACE_FILENAME}"    
+    
+    @property
+    def CODE_PROFILER_TRACE_NAME(self):
+        return f"{self.CODE_PROFILER_LOGS_DIR}/{self.CODE_PROFILER_TRACE_FILENAME}"    
     
     GUNICORN_LOGFILE_SIGNAL_HANDLER_INFO = "Worker.handle_usr1 of <gunicorn.workers.sync.SyncWorker object"
     INSTANCE_ID_ENV_NAME = "WEBSITE_INSTANCE_ID"
     
-    PID_FILE_LOCATION = f"{CODE_PROFILER_LOGS_DIR}/master_process.pid"
-    WEBSITE_HOSTNAME_ENV_NAME = "WEBSITE_INSTANCE_ID"  
+    @property
+    def PID_FILE_LOCATION(self):
+        instance_id = os.getenv(self.INSTANCE_ID_ENV_NAME,"default")
+        instance_id = instance_id[:4]
+        return f"{self.CODE_PROFILER_LOGS_DIR}/{instance_id}_master_process.pid"
+    
+    WEBSITE_HOSTNAME_ENV_NAME = "WEBSITE_HOSTNAME"  
+    LOCAL_DEVELOP_ENV_NAME = "APPSVC_PROFILER_USE_LOCAL_DEVELOPMENT"
+    LOCAL_DEVELOP_HOME_DIR = "APPSVC_PROFILER_LOCAL_DEVELOPMENT_HOME_DIR"
 
 class CodeProfilerExitCodes:
     Success=0    
